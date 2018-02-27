dfa65b468b90a82c0ced95fa3e07b02c33132755
AakashSYadav <anshulyadav1911@gmail.com>
2018-02-27 11:13:16 +0530

    initial commit

---
+++ Assets/Scripts/Roller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Roller : MonoBehaviour
+{
+    private Rigidbody rigidb;
+    public float speed;
+
+    private void Start()
+    {
+        rigidb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        Vector3 move = new Vector3(0, 0.0f, 100);
+        rigidb.AddForce(move*speed);
+    }
+}
